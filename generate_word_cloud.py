d594ca9dd69935640156d3eefdea3a5658f09c9c
olufemi-olumaiyegun <femimayegun@gmail.com>
2021-01-08 04:21:13 -0600

    fix

--- generate_word_cloud.py
+++ generate_word_cloud.py
@@ -1,19 +1,17 @@
-from wordcloud import WordCloud,STOPWORDS
-import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+uninteresting_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 words_file = open("words.txt", "r")
-stopwords = set(STOPWORDS)
 word_array = []
 word_count = {}
 for sentence in words_file:
     for word in sentence.split():
-        if word.isalpha():
+        word.lower()
+
+        if (word.isalpha() )and (word not in uninteresting_words):
             word_array.append(word)
             word_array.sort()
 for word in word_array:
     word_count[word] = word_array.count(word)
-print(word_count)
-cloud = WordCloud().generate_from_frequencies(word_count)
-plt.figure(figsize=(10,10),facecolor=None)
-plt.imshow(cloud)
-plt.axis("off")
-plt.show()
+cloud = WordCloud(width=1000,height=1000).generate_from_frequencies(word_count)
+cloud.to_file("cloud.jpg")
