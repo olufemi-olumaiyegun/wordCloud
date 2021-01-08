43e2a17b647b54426fe78b3de90430c6b70b4937
olufemi olumaiyegun <femimayegun@gmail.com>
2021-01-08 04:16:19 -0600

    now takes arguments

--- generate_word_cloud.py
+++ generate_word_cloud.py
@@ -1,6 +1,11 @@
 from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
-words_file = open("words.txt", "r")
+import argparse
+
+parser = argparse.ArgumentParser("description='file containing words to create cloud from'")
+parser.add_argument('filename')
+args = parser.parse_args()
+words_file = open(args.filename, "r")
 stopwords = set(STOPWORDS)
 word_array = []
 word_count = {}
@@ -11,8 +16,7 @@ for sentence in words_file:
             word_array.sort()
 for word in word_array:
     word_count[word] = word_array.count(word)
-print(word_count)
-cloud = WordCloud().generate_from_frequencies(word_count)
+cloud = WordCloud(width=1000,height=1000,background_color='white').generate_from_frequencies(word_count)
 plt.figure(figsize=(10,10),facecolor=None)
 plt.imshow(cloud)
 plt.axis("off")
